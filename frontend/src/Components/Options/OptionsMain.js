61a5c31641cdbb1672d8e43d534dbdbc81ab9bbc
Daniel Min <danielm710@gmail.com>
2020-01-21 16:21:26 -0500

    Removed unused code

--- frontend/src/Components/Options/OptionsMain.js
+++ frontend/src/Components/Options/OptionsMain.js
@@ -4,20 +4,6 @@ import './Options.css'
 import OptionItems from './OptionItems'
 
 function OptionsMain(props) {
-	const defaultOptionList = [
-		{
-			id: 0,
-			summaryText: "Basic Options",
-			defaultExpanded: true
-		},
-		{
-			id: 1,
-			summaryText: "Advanced Options",
-			detailText: "Advanded Option stuff goes here..."
-		}
-	]
-	const [ optionList, setOptionList ] = useState(defaultOptionList)
-
 	return(
 		<div className="option-container">
 			<OptionItems />
