7f8922434a5dc509dba5e039320aa18c8381e894
Daniel Min <danielm710@gmail.com>
2020-03-05 16:32:41 -0500

    using subprocess instead of module call for logfile issue

--- backend/AXIOME3_app/tasks/pipeline.py
+++ backend/AXIOME3_app/tasks/pipeline.py
@@ -1,14 +1,41 @@
 from AXIOME3_app.extensions import celery
 import luigi
-import pipeline # AXIOME3 Pipeline; its path shouldve been added
+#import pipeline # AXIOME3 Pipeline; its path shouldve been added
 import importlib
+import shutil
+import os
+import sys
+import subprocess
 
-@celery.task(bind=True, name="pipeline.run.import")
-def dummy_task(self):
+@celery.task(name="pipeline.run.import")
+def dummy_task():
+	# Path to configuration file to be used
+	#config_path = "/pipeline/AXIOME3/configuration/luigi.cfg"
+	#luigi.configuration.add_config_path(config_path)
+
 	# Reload pipeline to apply new config setting?
-	importlib.invalidate_caches()
-	importlib.reload(pipeline)
+	#importlib.invalidate_caches()
+	#importlib.reload(pipeline)
 
-	isSuccess = luigi.build([pipeline.Import_Data()], local_scheduler=True)
+	# Running luigi in python sub-shell so that each request can be logged in separate logfile.
+	# It's really hard to have separate logfile if running luigi as a module.
+	cmd = ["python", "/pipeline/AXIOME3/pipeline.py", "Import_Data", "--local-scheduler"]
+	proc = subprocess.Popen(cmd,
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE
+	)
+	stdout, stderr = proc.communicate()
 
-	return "Done!"
+	#isSuccess = luigi.build([pipeline.Import_Data()], local_scheduler=True)
+
+	# luigi log file path
+	log_file = "/pipeline/AXIOME3/luigi_log/luigi.log"
+	return log_file
+
+@celery.task(name="pipeline.run.move")
+def move_log_task(original_log, _id):
+	new_log = os.path.join("/log", _id, "luigi.log")
+
+	shutil.move(original_log, new_log)
+
+	return "Moved!"
