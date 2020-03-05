e4d3053634e74c5afb3e43bd6e2cda2f11f045c1
Daniel Min <danielm710@gmail.com>
2020-03-05 16:31:40 -0500

    refactored

--- backend/AXIOME3_app/datahandle/views.py
+++ backend/AXIOME3_app/datahandle/views.py
@@ -12,7 +12,7 @@ from AXIOME3_app.datahandle import luigi_prep_helper
 from AXIOME3_app.datahandle import config_generator
 
 # Celery task
-from AXIOME3_app.tasks.pipeline import dummy_task
+from AXIOME3_app.tasks.pipeline import dummy_task, move_log_task
 
 # Messages
 from AXIOME3_app.messages.message import (
@@ -21,8 +21,20 @@ from AXIOME3_app.messages.message import (
 
 blueprint = Blueprint("datahandle", __name__, url_prefix="/datahandle")
 
-def on_raw_message(body):
-	print(body, file=sys.stderr)
+def responseIfError(func, **kwargs):
+	"""
+	Executes a custom function, and returns an appropriate response
+	if there is an error.
+
+	Input:
+		func: A function that returns status code and result.
+		kwargs: keyword arguments to be passed to a custom function.
+	"""
+	code, result = func(**kwargs)
+	if(code != 200):
+		return Response(INTERNAL_SERVER_ERROR_MESSAGE, status=500, mimetype='text/html')
+
+	return result
 
 @blueprint.route("/", methods=['POST'])
 def generate_files():
@@ -32,29 +44,22 @@ def generate_files():
 	_id = str(uuid.uuid4())
 
 	# Make sub output dir in /output
-	code, message = luigi_prep_helper.make_output_dir(_id)
-	if(code != 200):
-		return Response(INTERNAL_SERVER_ERROR_MESSAGE, status=code, mimetype='text/html')
+	responseIfError(luigi_prep_helper.make_output_dir, _id=_id)
 
 	# Make sub output dir in /output
-	code, message = luigi_prep_helper.make_log_dir(_id)
-	if(code != 200):
-		return Response(INTERNAL_SERVER_ERROR_MESSAGE, status=code, mimetype='text/html')
+	responseIfError(luigi_prep_helper.make_log_dir, _id=_id)
 
 	# Create luigi logging config file
-	code, result = config_generator.make_log_config(_id)
-	if(code != 200):
-		return Response(INTERNAL_SERVER_ERROR_MESSAGE, status=code, mimetype='text/html')
+	log_config_path = responseIfError(config_generator.make_log_config, _id=_id)
 
 	# TODO
 	# Store received files?
 	# Make config file for luigi
 	if(form_type == "InputUpload"):
 		code, manifest_path = luigi_prep_helper.save_upload(_id, request.files["manifest"])
-		new_manifest_path = luigi_prep_helper.reformat_manifest(_id, manifest_path)
-		code, config_path = config_generator.make_luigi_config(_id, result, manifest=new_manifest_path)
+		code, new_manifest_path = luigi_prep_helper.reformat_manifest(_id, manifest_path)
+		code, config_path = config_generator.make_luigi_config(_id, log_config_path, manifest=new_manifest_path)
 
-	time.sleep(0.5)
 	dummy_task.apply_async()
 
 	return Response("ok", status=200, mimetype='text/html')
