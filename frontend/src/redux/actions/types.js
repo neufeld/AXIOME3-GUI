968e018b12b74cc24317f3b13f75fe943850386c
Daniel Min <danielm710@gmail.com>
2020-03-12 15:45:39 -0400

    more action types added

--- frontend/src/redux/actions/types.js
+++ frontend/src/redux/actions/types.js
@@ -2,9 +2,13 @@
 export const GET_FILES = 'GET_FILES'
 export const GET_UPLOAD_FIELD = 'GET_UPLOAD_FIELD'
 export const SELECT_FILE = 'SELECT_FILE'
+export const RESET_SELECTED_FILES = 'RESET_SELECTED_FILES'
 // tabAction related
 export const UPDATE_TAB = 'UPDATE_TAB'
 // optionAction related
 export const UPDATE_OPTIONS = 'UPDATE_OPTIONS'
+export const SELECT_OPTIONS = 'SELECT_OPTIONS'
+export const RESET_SELECTED_OPTIONS = 'RESET_SELECTED_OPTIONS'
+export const RESET_OPTIONS = 'RESET_OPTIONS'
 // submitAction related
 //export const SUBMIT = 'SUBMIT'
