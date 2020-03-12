1a680701c05e70b3edcdc4e284d6133732c9d033
Daniel Min <danielm710@gmail.com>
2020-03-12 15:44:56 -0400

    select option action added

--- frontend/src/redux/actions/optionAction.js
+++ frontend/src/redux/actions/optionAction.js
@@ -1,4 +1,4 @@
-import { UPDATE_OPTIONS } from './types';
+import { UPDATE_OPTIONS, SELECT_OPTIONS, RESET_SELECTED_OPTIONS, RESET_OPTIONS } from './types';
 
 export const updateOptionList = (options) => dispatch => {
 	dispatch({
@@ -6,5 +6,27 @@ export const updateOptionList = (options) => dispatch => {
 		payload: {
 			options: options
 		}
+	})
+}
+
+export const selectOptions = (optionName, optionValue) => dispatch => {
+	dispatch({
+		type: SELECT_OPTIONS,
+		payload: {
+			name: optionName,
+			value: optionValue
+		}
+	})
+}
+
+export const resetOptions = () => (dispatch) => {
+	dispatch({
+		type: RESET_OPTIONS
+	})
+}
+
+export const resetSelectedOptions = () => (dispatch, getState) => {	
+	dispatch({
+		type: RESET_SELECTED_OPTIONS
 	})
 }
