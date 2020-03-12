ed397ab95561729625d3eaacd47cab55374e1e2a
Daniel Min <danielm710@gmail.com>
2020-03-12 15:45:18 -0400

    select option reducer added & reset reducer added

--- frontend/src/redux/reducers/optionReducer.js
+++ frontend/src/redux/reducers/optionReducer.js
@@ -1,7 +1,10 @@
-import { UPDATE_OPTIONS } from '../actions/types';
+import { UPDATE_OPTIONS, SELECT_OPTIONS, RESET_SELECTED_OPTIONS, RESET_OPTIONS } from '../actions/types';
 
 const initialState = {
-	options: {}
+	options: {
+		
+	},
+	selectedOptions: {}
 }
 
 export default function(state = initialState, action) {
@@ -10,6 +13,27 @@ export default function(state = initialState, action) {
 			return {
 				...state,
 				options: action.payload.options
+			}
+
+		case SELECT_OPTIONS:
+			return {
+				...state,
+				selectedOptions: {
+					...state.selectedOptions,
+					[action.payload.name]: action.payload.value
+				}
+			}
+
+		case RESET_SELECTED_OPTIONS:
+			return {
+				...state,
+				selectedOptions: {...initialState.selectedOptions }
+			}
+
+		case RESET_OPTIONS:
+			return {
+				...state,
+				options: { ...initialState.options }
 			}
 
 		default:
