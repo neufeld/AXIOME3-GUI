f7b067345a835ab2c42fc19e81832826e6d18da8
Daniel Min <danielm710@gmail.com>
2020-03-12 15:51:28 -0400

    adding options to form data

--- frontend/src/Components/SubmitButton/SubmitHelper.js
+++ frontend/src/Components/SubmitButton/SubmitHelper.js
@@ -8,15 +8,18 @@ import axios from 'axios'
  *	- e: which form is being submitted? (e.g. InputUpload, Denoise, Analysis)
  */
 
- const handleSubmit = async (e, formType, selectedFiles, options) => {
+ const handleSubmit = async (e, formType, selectedFiles, selectedOptions) => {
  	e.preventDefault();
 
  	const endpoint = '/datahandle/'
-
  	const formData = new FormData();
 
  	// Form Type
  	formData.append("formType", formType)
+
+ 	Object.keys(selectedOptions).forEach(k => {
+ 		formData.append(k, selectedOptions[k])
+ 	});
 
  	if(formType === "InputUpload") {
  		// Check if "empty" form
@@ -30,8 +33,6 @@ import axios from 'axios'
  		formData.append("manifest", selectedFiles[0].selectedFile)
  	}
 
- 	//TODO
- 	// Check formData properly filled out?
  	try {
  		const res = await axios.post(endpoint, formData, {
 	 		headers: {
@@ -40,7 +41,21 @@ import axios from 'axios'
 	 	})
 	 	console.log(res)
  	} catch (err) {
-		console.log(err)
+ 		if(err.response) {
+ 			// Catching custom error codes
+ 			const client_error = /\b[4][0-9]{2}\b/g;
+ 			const server_error = /\b[5][0-9]{2}\b/g;
+ 			if(err.response.status.toString().match(client_error)) {
+ 				alert(err.response.data)
+ 			} else if(err.response.status.toString().match(server_error)) {
+ 				alert("Internal server error...")
+ 			}
+ 		}
+		else {
+			alert("Server unexpectedly failed...")
+			console.log(err.response)
+		}
+ 		console.log(err.response)
 	}
 
  }
