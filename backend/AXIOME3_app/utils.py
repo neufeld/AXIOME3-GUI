fa834da3e1e4c839904daec82f9c6d17088876e7
Daniel Min <danielm710@gmail.com>
2020-03-12 15:44:19 -0400

    all the logging happens from here now

--- backend/AXIOME3_app/utils.py
+++ backend/AXIOME3_app/utils.py
@@ -3,6 +3,7 @@ This is strictly for flask related util (helper) functions.
 For example, error handling.
 """
 from flask import Response
+from flask import current_app
 
 from AXIOME3_app.exceptions.exception import CustomError
 
@@ -17,15 +18,20 @@ def responseIfError(func, **kwargs):
 	If error, raises an exception with appropriate response.
 
 	Input:
-		func: A function that returns status code and result.
+		func: A function to be run. MUST return status code and result (of any type).
 		kwargs: keyword arguments to be passed to the custom function.
 	"""
 	code, result = func(**kwargs)
 	if(code != 200):
+		# Log the error message
+		current_app.logger.error(result)
+
 		if(str(code).startswith("5")):
 			response = Response(INTERNAL_SERVER_ERROR_MESSAGE, status=code, mimetype='text/html')
 
 		elif(str(code).startswith("4")):
+			# Remove "/hostfs" if any
+			result = result.replace("/hostfs", "")
 			response = Response(result, status=code, mimetype='text/html')
 
 		raise CustomError(result, response)
