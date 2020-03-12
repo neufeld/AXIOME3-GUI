1cf66a80b9b7edadb250c17af6e7c3a7087cc892
Daniel Min <danielm710@gmail.com>
2020-03-12 15:47:19 -0400

    various types of options supported

--- frontend/src/Components/Options/OptionInputField.js
+++ frontend/src/Components/Options/OptionInputField.js
@@ -1,12 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-function OptionInputField(props) {
+import { selectOptions } from '../../redux/actions/optionAction'
+
+export function OptionInputField(props) {
+	// type: input tag type
+	// dropdownOption: options to be used for dropdown menu
+	// name: name of the option (to update redux state)
+	const { type, dropdownOption = [], name, defaultValue, options } = props;
+
+	// Redux action
+	const { selectOptions } = props;
+
+	let inputField;
+	// Initially update redux state with the default value 
+	useEffect(() => {
+		//console.log(options)
+		selectOptions(name, defaultValue)
+	}, [options])
+
+	if(type === "dropdown") {
+		const options = dropdownOption.map(option => {
+			return (
+				<option value={option} key={option}>{option}</option>
+			)
+		})
+
+		inputField = <select
+										className="styled-dropdown"
+										defaultValue={defaultValue}
+										onChange={e => {selectOptions(name, e.target.value)}}
+									>
+									{options}
+									</select>
+	} else {
+		inputField = <input
+										type={type}	
+										onChange={e => {selectOptions(name, e.target.value)}}
+									>
+									</input>
+	}
 
 	return(
 		<div className="option-input-container">
-			<input type="number"></input>
+			{inputField}
 		</div>
 	)
 }
 
-export default OptionInputField
+const mapStateToProps = state => ({
+	selectedOptions: state.option.selectedOptions,
+	options: state.option.options
+})
+
+const mapDispatchToProps = { selectOptions }
+
+export default connect(mapStateToProps, mapDispatchToProps)(OptionInputField)
