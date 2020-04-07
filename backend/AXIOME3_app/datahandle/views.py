ee519a63db6df50e42b979a4314f38d3fded6f5b
Daniel Min <danielm710@gmail.com>
2020-04-07 13:51:10 -0400

    webscoket integration

--- backend/AXIOME3_app/datahandle/views.py
+++ backend/AXIOME3_app/datahandle/views.py
@@ -1,11 +1,9 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, current_app
 import uuid
 import os
 #from werkzeug import secure_filename
 # For console debugging
 import sys
-import time
-import random
 
 # Custom modules
 from AXIOME3_app.datahandle import luigi_prep_helper
@@ -17,6 +15,7 @@ from AXIOME3_app.tasks.pipeline import import_data_task
 # Custom Exceptions
 from AXIOME3_app.exceptions.exception import CustomError
 
+import redis
 
 blueprint = Blueprint("datahandle", __name__, url_prefix="/datahandle")
 
@@ -32,6 +31,7 @@ def generate_files():
 			manifest_file = request.files["manifest"]
 			input_format = request.form["Input Format"]
 			sample_type = request.form["Sample Type"]
+
 			# Do preliminary checks on manifest file
 			manifest_path = luigi_prep_helper.input_upload_pre_check(
 				_id=_id,
@@ -48,27 +48,44 @@ def generate_files():
 				input_format=input_format
 			)
 			# Run the pipeline
-			import_data_task.apply_async(args=[_id])
+			import_data_task.apply_async(args=[_id, current_app.config["CELERY_BROKER_URL"]])
 
 	except CustomError as err:
 		return err.response
 
-	return Response("ok", status=200, mimetype='text/html')
+	return Response(current_app.config["CELERY_BROKER_URL"], status=200, mimetype='text/html')
 
-@blueprint.route("/stream/")
-def stream():
-	def eventstream():
-		while True:
-			time.sleep(5);
-			n = random.random()
-			data = "data: This number, {}, is from the server!\n\n".format(str(n))
-			yield data
+#@socketio.on('')
 
-	res = Response(eventstream(), mimetype="text/event-stream")
-	# As of March 2020, there seems to be a bug with SSE in React
-	# Need to set Cache-Control: no-transform if using proxy url
-	# full URL would still work without this header.
-	res.headers['Cache-Control'] = 'no-transform'
-	
-	return res
-	#return "stream!"
+#@blueprint.route("/stream/")
+#def stream():
+#	def eventstream():
+#		while True:
+#			time.sleep(5)
+#			n = random.random()
+#			data = "data: This number, {}, is from the server!\n\n".format(str(n))
+#			yield data
+#
+#	res = Response(eventstream(), mimetype="text/event-stream")
+#	# As of March 2020, there seems to be a bug with SSE in React
+#	# Need to set Cache-Control: no-transform if using proxy url
+#	# full URL would still work without this header.
+#	res.headers['Cache-Control'] = 'no-transform'
+#	
+#	return res
+#	#return "stream!"
+
+#def event_stream():
+#	r = redis.StrictRedis(host='redis', port=6379)
+#	pubsub = r.pubsub(ignore_subscribe_messages=True)
+#	pubsub.subscribe('test')
+#	# TODO: handle client disconnection.
+#	for message in pubsub.listen():
+#		yield 'data: %s\n\n' % message['data']
+#
+#@blueprint.route("/stream/")
+#def stream():
+#	res = Response(event_stream(), mimetype="text/event-stream")
+#	res.headers['Cache-Control'] = 'no-transform'
+#
+#	return res
