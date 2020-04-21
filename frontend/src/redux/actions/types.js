49b9fee755a00ebab74e66a02f9b938041911989
Daniel Min <danielm710@gmail.com>
2020-04-21 14:20:31 -0400

    Added download types

--- frontend/src/redux/actions/types.js
+++ frontend/src/redux/actions/types.js
@@ -10,5 +10,7 @@ export const UPDATE_OPTIONS = 'UPDATE_OPTIONS'
 export const SELECT_OPTIONS = 'SELECT_OPTIONS'
 export const RESET_SELECTED_OPTIONS = 'RESET_SELECTED_OPTIONS'
 export const RESET_OPTIONS = 'RESET_OPTIONS'
-// submitAction related
-//export const SUBMIT = 'SUBMIT'
+// downloadAction related
+export const GET_DOWNLOAD_PATH = 'GET_DOWNLOAD_PATH'
+export const RESET_DOWNLOAD_PATH = 'RESET_DOWNLOAD_PATH'
+export const GET_UID = 'GET_UID'
