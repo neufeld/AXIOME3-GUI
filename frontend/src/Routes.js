2e5d6df6813c28e6961795ec2b2e13858d2af35e
Daniel Min <danielm710@gmail.com>
2020-04-21 14:10:15 -0400

    separate route file

---
+++ frontend/src/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store'
+
+import AXIOME3 from './AXIOME3'
+import Report from './Report'
+
+const Routes = () => (
+	<Provider store={store}>
+		<Router>
+			<div>
+				<Switch>
+					<Route exact path="/" component={AXIOME3} />
+					<Route exact path="/tmp" component={Report} />
+				</Switch>
+			</div>
+		</Router>
+	</Provider>
+);
+
+export default Routes
