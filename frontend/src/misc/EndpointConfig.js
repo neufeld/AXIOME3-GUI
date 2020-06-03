e260ecb400554dcde491ccba962736f62ef1bf04
Daniel Min <danielm710@gmail.com>
2020-06-03 18:23:23 -0400

    API endpoints and form types as config

---
+++ frontend/src/misc/EndpointConfig.js
@@ -0,0 +1,6 @@
+// Custom PCoA endpoints
+export const CUSTOM_PCOA_PNG_ENDPOINT = '/custompcoa/png';
+export const CUSTOM_PCOA_PDF_ENDPOINT = '/custompcoa/pdf';
+// Bubbleplot endpoints
+export const BUBBLEPLOT_PNG_ENDPOINT = '/bubbleplot/png';
+export const BUBBLEPLOT_PDF_ENDPOINT = '/bubbleplot/pdf';
---
+++ frontend/src/misc/FormTypeConfig.js
@@ -0,0 +1,5 @@
+export const INPUT_UPLOAD_FORMTYPE = 'inputUpload';
+export const DENOISE_FORMTYPE = 'denoise';
+export const ANALYSIS_FORMTYPE = 'analysis';
+export const PCOA_FORMTYPE = 'pcoa';
+export const BUBBLEPLOT_FORMTYPE = 'bubbleplot';
