80fd6cf976fa4ce2cd6a1669ecb083dc31707847
Daniel Min <danielm710@gmail.com>
2020-06-10 20:08:50 -0400

    utils function added

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,6 +1,18 @@
+// Open URL in a new tab
 export const openInNewTab = (URL) => {
 	const win = window.open(URL, '_blank');
 	if(win != null) {
 		win.focus();
 	}
+}
+
+// Convert buffer array to base-64
+export const convertArrayBufferToBase64 = (arrayBuffer) => {
+	const base64 = btoa(
+		new Uint8Array(arrayBuffer).reduce( 
+			(data, byte) => data + String.fromCharCode(byte), '' 
+		)
+	)
+
+	return base64
 }
