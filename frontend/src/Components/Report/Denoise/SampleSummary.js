3661402eb2d4360c4f40f6000bcf0417d7028a5b
Daniel Min <danielm710@gmail.com>
2020-06-12 06:44:12 -0400

    Sample count summary table

---
+++ frontend/src/Components/Report/Denoise/SampleSummary.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+import TableMain from '../../Table/TableMain';
+
+function SampleSummary(props) {
+	// Redux state
+	const { uid } = props;
+
+	// Local state
+	const [ tableData, setTableData ] = useState([]);
+
+	useEffect(() => {
+		const denoiseSampleSummaryEndpoint = '/denoise/sample_summary/json';
+
+		const sampleSummaryApiRequest = async () => {
+			const formData = new FormData();
+			const data	= [];
+
+			formData.append('uid', uid);
+
+			const configOptions = {
+				url: denoiseSampleSummaryEndpoint,
+				method: 'post',
+				data: formData,
+				responseType: 'json'
+			}
+			const res = await axios(configOptions);
+			const summaryCounts = res.data;
+
+			Object.keys(summaryCounts).forEach(sampleId => {
+				const row = {
+					sampleId: sampleId,
+					sampleCount: summaryCounts[sampleId],
+				}
+				data.push(row);
+			})
+			setTableData(data);
+		}
+
+		sampleSummaryApiRequest();
+	}, [])
+
+	const columns = [
+		{
+			Header: 'Sample ID',
+			accessor: 'sampleId',
+		},
+		{
+			Header: 'Sample Count',
+			accessor: 'sampleCount',
+		},
+	]
+
+	return(
+		<div>
+			<TableMain columns={columns} data={tableData} />
+		</div>
+	)
+}
+const mapStateToProps  = state => ({
+	uid: state.submit.uid
+})
+
+const mapDispatchToProps = {
+	
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SampleSummary)
