26ba23a21e5c990020b4de1fea9fb295fe89393d
Daniel Min <danielm710@gmail.com>
2020-06-18 03:59:42 -0400

    using RabbitMQ as backend for celery

--- backend/AXIOME3_app/config.py
+++ backend/AXIOME3_app/config.py
@@ -1,2 +1,6 @@
-CELERY_BROKER_URL='redis://redis:6379/0'
-CELERY_RESULT_BACKEND='redis://redis:6379/0'
+# Redis
+#CELERY_BROKER_URL='redis://redis:6379/0'
+#CELERY_RESULT_BACKEND='redis://redis:6379/0'
+# RabbitMQ
+CELERY_BROKER_URL='pyamqp://admin:mypass@rabbit//'
+CELERY_RESULT_BACKEND='rpc://'
