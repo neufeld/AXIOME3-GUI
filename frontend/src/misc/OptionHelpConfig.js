ad3317593d3cdbde906f206b92781847c629e292
Daniel Min <danielm710@gmail.com>
2020-07-31 15:16:13 -0400

    minor change

--- frontend/src/misc/OptionHelpConfig.js
+++ frontend/src/misc/OptionHelpConfig.js
@@ -27,7 +27,7 @@ export const LEGEND_TEXT_SIZE_HELP = "Legend content size."
 export const ORDINATION_COLLAPSE_LEVEL_HELP = "Taxonomic level at which to collapse feature table when projecting onto ordination space"
 export const WEIGHTED_AVERAGE_COLLAPSE_LEVEL_HELP = "Taxonomic level at which to collapse feature table when projecting as different bubbles"
 export const DISSMILARITY_INDEX_HELP = "Dissimilarity index metric"
-export const ABUNDANCE_THRESHOLD_HELP = "Keep ASV/taxa if at least one of the samples have % abundance greather than this value"
+export const ABUNDANCE_THRESHOLD_HELP = "Keep samples if at least one of the ASV/taxa in the sample has % abundance greather than this value"
 export const R2_THRESHOLD_HELP = "Environmental vector arrows with R2 value greater than the threshold will be displayed"
 export const WA_THRESHOLD_HELP = "Taxa bubbles with normalized abundance greather than this value will be displayed"
 // Details
