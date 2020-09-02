7594b6bd42e7dd7d886d614a176134f7d9d54bd0
Daniel Min <danielm710@gmail.com>
2020-09-01 18:11:23 -0700

    sendgrid api added

--- backend/AXIOME3_app/config.py
+++ backend/AXIOME3_app/config.py
@@ -1,3 +1,4 @@
+import os
 # Redis
 #CELERY_BROKER_URL='redis://redis:6379/0'
 #CELERY_RESULT_BACKEND='redis://redis:6379/0'
@@ -9,13 +10,13 @@ CELERY_BROKER_URL='pyamqp://axiome3:neufeld@rabbit/axiome3_host'
 CELERY_RESULT_BACKEND='rpc://'
 
 # Flask Mail
-MAIL_DEFAULT_SENDER="axiome333@gmail.com"
-MAIL_USERNAME="axiome333@gmail.com"
-MAIL_PASSWORD="neufeldlab333"
-MAIL_SERVER="smtp.gmail.com"
-MAIL_PORT=465
-MAIL_USE_TLS=False
-MAIL_USE_SSL=True
+MAIL_USERNAME="apikey"
+MAIL_SERVER="smtp.sendgrid.net"
+MAIL_DEFAULT_SENDER=os.environ.get('MAIL_DEFAULT_SENDER')
+MAIL_PASSWORD=os.environ.get('SENDGRID_API_KEY')
+MAIL_PORT=587
+MAIL_USE_TLS=True
+MAIL_USE_SSL=False
 
 # Default classifier path
 # QIIME2 2020.6 SILVA 138
