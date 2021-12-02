6c07e8785afe1598ed7a5a7e23da6c4533bdff09
Daniel Min <danielmin@MOMAC99003.hitronhub.home>
2021-12-01 21:55:31 -0800

    bug fix

--- backend/AXIOME3_app/report/combined_asv_table/views.py
+++ backend/AXIOME3_app/report/combined_asv_table/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, send_file
 import os
+from AXIOME3_app.utils import get_analysis_dir
 
 blueprint = Blueprint("combined_asv_table", __name__, url_prefix="/combined_asv_table")
 
@@ -11,6 +12,7 @@ def combined_table_tsv():
 		# return sample output if uid not specified
 		combined_table = os.path.join('/data/output/exported', 'ASV_table_combined.tsv')
 	else:
-		combined_table = os.path.join('/output', uid, 'exported', 'ASV_table_combined.tsv')
+		ANALYSIS_DIR = get_analysis_dir(uid)
+		combined_table = os.path.join(ANALYSIS_DIR, 'ASV_table_combined.tsv')
 
 	return send_file(combined_table, mimetype='text/tab-separated-values', as_attachment=True)
