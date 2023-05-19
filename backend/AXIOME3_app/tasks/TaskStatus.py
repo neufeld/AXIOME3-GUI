1e38f40bb309d3b73ac88f094ec2ebed77de0034
Kevin Shen <kshen@neufeldserver.uwaterloo.ca>
2023-05-19 15:45:44 -0400

    Created env variables to separate out test and official copies of AXIOME

--- backend/AXIOME3_app/tasks/TaskStatus.py
+++ backend/AXIOME3_app/tasks/TaskStatus.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 class TaskStatus(object):
 	def __init__(self):
@@ -14,16 +15,20 @@ class TaskStatus(object):
 	def _browse_queue(queue_name: str):
 		"""Browse rabbit mq queue. It's likely O(n) so not so scalable..."""
 		# Technically should hide credentials lol
-		# r = requests.post(
-		# 	f'http://rabbit:15672/api/queues/axiome3_host/{queue_name}/get',
-		# 	auth=('axiome3', 'neufeld'),
-		# 	data=json.dumps(self.payload),
-		# )
-		r = requests.post(
-			f'http://rabbit2:15672/api/queues/axiome3_host/{queue_name}/get',
-			auth=('axiome3', 'neufeld'),
-			data=json.dumps(self.payload),
-		)
+		r = "UNDEFINED"
+		if os.environ.get("RUN_ENV") == "test":
+			r = requests.post(
+				f'http://rabbit2:15672/api/queues/axiome3_host/{queue_name}/get',
+				auth=('axiome3', 'neufeld'),
+				data=json.dumps(self.payload),
+			)
+		elif os.environ.get("RUN_ENV") == "official":
+			r = requests.post(
+				f'http://rabbit:15672/api/queues/axiome3_host/{queue_name}/get',
+				auth=('axiome3', 'neufeld'),
+				data=json.dumps(self.payload),
+			)
+
 
 		return r.json()
 
--- frontend/src/misc/apiConfig.js
+++ frontend/src/misc/apiConfig.js
@@ -4,7 +4,13 @@ const hostname = window && window.location && window.location.hostname;
 
 // Add different endpoints for different hostnames
 // Nginx was configured to redirect requests to /api to backend service
-baseEndpoint = 'http://localhost:8081'
-// baseEndpoint = 'http://localhost:8080'
+baseEndpoint = "UNDEFINED1"
+
+if(process.env.REACT_APP_RUN_ENV == "test"){
+    baseEndpoint = 'http://localhost:8081'
+}
+else if(process.env.REACT_APP_RUN_ENV == "official"){
+    baseEndpoint = 'http://localhost:8080'
+}
 
 export const ENDPOINT_ROOT = `${baseEndpoint}/api`
